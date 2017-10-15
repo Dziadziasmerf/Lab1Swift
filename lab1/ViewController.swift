740bf8819f2d8ac6f2db97ae5e05c4d566c8ac85
DziadziaS <dziadziasmerf@DziadziaSs-iMac.local>
2017-10-15 15:18:00 -0700

    Added getting albums data

--- lab1/ViewController.swift
+++ lab1/ViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+struct Album : Decodable {
+    let album : String
+    let artist : String
+    let genre : String
+    let tracks : Int
+    let year : Int
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet var wykonawcaTF : UITextField?
@@ -15,12 +23,13 @@ class ViewController: UIViewController {
     @IBOutlet var gatunekMuzycznyTF : UITextField?
     @IBOutlet var rokWydaniaTF : UITextField?
     @IBOutlet var liczbaSciezekNaPlycie : UITextField?
-    
+    var albums : [Album]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        getRecords()
+        // Do any additional setup after loading the view, typically from a nib.
         
     }
 
@@ -29,6 +38,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getRecords() {
+        guard let url = URL(string: "https://isebi.net/albums.php") else {return}
+        let session = URLSession.shared
+        session.dataTask(with: url) {  (albums, response, error) in
+            if let albums = albums {
+                print(albums)
+                do {
+                    self.albums = try JSONDecoder().decode([Album].self, from: albums)
+                    print(self.albums![1].album)
+                    print(self.albums![1].artist)
+                    
+                } catch let jsonError {
+                        print(jsonError)
+                }
+            }
+        }.resume()
+    
+    }
     
 }
-
